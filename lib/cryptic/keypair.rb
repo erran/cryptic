7f5e8860d1243ac61943f5e100343e0a861eb912
Erran Carey <me@errancarey.com>
2013-07-05 23:29:27 -0500

    Update Cryptic::Keypair and 'cryptic generate' cmd

--- lib/cryptic/keypair.rb
+++ lib/cryptic/keypair.rb
@@ -12,57 +12,50 @@ module Cryptic
   # @!attribute [String] public_key the contents of the public key file
   # @author Erran Carey <me@errancarey.com>
   class Keypair
-    # Creates a keypair to be saved with #save or #save!
+    # Creates a keypair to be saved with #save
     #
     # @param [Fixnum] size the amount of bits to use in your key
-    # @param [Fixnum] exponent the exponent to use when generating your key
     # @return [Keypair] an object representing a private/public keypair
-    def initialize(size = 2048, exponent = 65537)
-      keypair = OpenSSL::PKey::RSA.generate(size, exponent)
+    def initialize(passphrase = nil, size = 2048)
+      @saved = false
 
-      @private_key = keypair.to_s
-      @public_key = keypair.public_key.to_s
+      attempts ||= 0
+      attempts += 1
+
+      rsa_key = OpenSSL::PKey::RSA.new(size)
+      cipher = OpenSSL::Cipher::AES256.new(:CBC)
+
+      @private_key = rsa_key.to_pem(cipher, passphrase)
+      @public_key = rsa_key.public_key.to_pem
+    rescue OpenSSL::PKey::RSAError => e
+      if e.message =~ /^read key$/
+        retry unless attempts > 1
+      else
+        raise e
+      end
     end
 
-    # Saves the private and public keypair
+    # Save the file
     #
     # @param [String] path the path to save the keypair into
-    # @return [false,String] returns false on failure and returns the path
-    #   parameter on success
-    def save(path = '.')
-      FileUtils.mkdir_p(File.dirname(path))
-
-      File.open("#{path}/cryptic_private.pem", File::WRONLY|File::CREAT|File::EXCL) do |file|
-          file.write @private_key
-      end
-
-      File.open('cryptic_public.pem', File::WRONLY|File::CREAT|File::EXCL) do |file|
-          file.write @public_key
-      end
-
-      path
-    rescue SystemCallError => e
-      $stderr.puts e
-      false
-    end
-
-    # Forcefully saves the private and public keys and allows exceptions to be
-    # raised
-    #
-    # @param [String] path the path to save the keypair into
-    # @todo Document what save! will raise
+    # @todo Document what save may raise
     # @return [String] returns the path files were saved to
-    def save!(path = '.')
+    def save(path = '.')
+      if @private_key.to_s.eql?('') || @public_key.to_s.eql?('')
+        raise Cryptic::KeyGenerationFailure, "The keypair was never successfully generated"
+      end
+
       FileUtils.mkdir_p(File.dirname(path))
 
-      File.open("#{path}/cryptic_private.pem", 'w') do |file|
+      File.open("#{File.expand_path(path)}/cryptic_private.pem", 'w') do |file|
         file.write @private_key
       end
 
-      File.open("#{path}/cryptic_public.pem", 'w') do |file|
+      File.open("#{File.expand_path(path)}/cryptic_public.pem", 'w') do |file|
         file.write @public_key
       end
 
+      @saved = true
       path
     end
   end
