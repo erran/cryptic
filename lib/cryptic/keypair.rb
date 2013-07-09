98d115fd723e63737554bca2f77a89405c0a7979
Erran Carey <me@errancarey.com>
2013-07-09 01:26:54 -0500

    Update key generation steps

--- lib/cryptic/keypair.rb
+++ lib/cryptic/keypair.rb
@@ -16,7 +16,7 @@ module Cryptic
     # Initializes a Cryptic::Keypair object from a private/public keypair
     #
     # @note The passphrase isn't saved
-    # @todo Use the Ruby 2 named parameters syntax vs. an opts hash
+    # @todo Code clean up between initialize and generate!
     # @todo Validate that keys are public/private
     # @param [String] private_key the private key (or file) to use
     # @param [Hash] opts additional options to configure your Keypair with
@@ -25,32 +25,41 @@ module Cryptic
     #   from
     # @return [Keypair] the initialized Cryptic::Keypair object
     def initialize(private_key, opts = { public_key: nil, passphrase: nil })
-      @private_key = OpenSSL::PKey::RSA.new(
-                       if File.exists?(private_key)
-                         File.read(private_key)
-                       else
-                         private_key
-                       end,
-                       opts[:passphrase]
-                     )
-
-      unless @private_key.private?
-        raise(
-          Cryptic::InvalidKey,
-          "Public key '#{private_key}' provided as a private key."
-        )
+      if private_key.is_a? OpenSSL::PKey::RSA
+        @private_key = private_key
+      elsif !private_key.to_pem.eql? ''
+        @private_key = OpenSSL::PKey::RSA.new(
+                         if File.exists?(private_key)
+                           File.read(private_key)
+                         else
+                           private_key
+                         end,
+                         opts[:passphrase]
+                       )
+        unless @private_key.private?
+          raise(
+            Cryptic::InvalidKey,
+            "Public key '#{private_key}' provided as a private key."
+          )
+        end
       end
 
-      @public_key = OpenSSL::PKey::RSA.new(
-                      if opts[:public_key] && File.exists?(opts[:public_key])
-                        File.read(opts[:public_key])
-                      elsif opts[:public_key]
-                        opts[:public_key]
-                      else
-                        @private_key
-                      end,
-                      opts[:passphrase]
-                    ).public_key
+      if public_key.is_a? OpenSSL::PKey::RSA
+        @public_key = public_key
+      else
+        @public_key = OpenSSL::PKey::RSA.new(
+                        if opts[:public_key] && File.exists?(opts[:public_key].to_s)
+                          File.read(opts[:public_key])
+                        elsif opts[:public_key]
+                          opts[:public_key]
+                        else
+                          require 'pry'
+                          binding.pry
+                          @private_key or raise(Cryptic::KeyNotFound)
+                        end,
+                        opts[:passphrase]
+                      ).public_key
+      end
     rescue OpenSSL::PKey::RSAError => e
       if e.message.eql? 'Neither PUB key nor PRIV key:: not enough data'
         if @private_key.nil?
@@ -73,16 +82,21 @@ module Cryptic
     # @param [Fixnum] size the amount of bits to use in your key
     # @return [Keypair] an object representing a private/public keypair
     def self.generate(passphrase = nil, size = 2048)
-      # OPTIMIZE: There are so many hammers in here it looks like we're playing
-      # Whack-a-mole
       # TODO: Find a better way to handle retries
       attempts ||= 0
       attempts += 1
 
-      rsa_key = OpenSSL::PKey::RSA.new(size)
-      cipher ||= OpenSSL::Cipher::AES256.new(:CBC)
+      rsa_key = OpenSSL::PKey::RSA.new(size, passphrase)
+      cipher = OpenSSL::Cipher::AES256.new(:CBC)
 
-      new(rsa_key.to_pem(cipher, passphrase), { public_key: rsa_key.public_key.to_pem, passphrase: passphrase })
+      new(
+        rsa_key,
+        {
+          cipher: cipher,
+          public_key: rsa_key.public_key,
+          passphrase: passphrase
+        }
+      )
     rescue OpenSSL::PKey::RSAError => e
       if e.message =~ /^read key$/
         retry unless attempts > 1
@@ -97,19 +111,22 @@ module Cryptic
     # @todo Document what save may raise
     # @todo Update this method
     # @return [String] returns the path files were saved to
-    def save(path = '.')
-      if @private_key.to_pem.eql?('') || @public_key.to_pem.eql?('')
+    def save(path = '.', opts = { cipher: OpenSSL::Cipher::AES256.new(:CBC), passphrase: nil})
+      priv = @private_key.to_pem(opts[:cipher], opts[:passphrase])
+      pub = (opts[:public_key] || @public_key || @private_key.public_key).to_pem
+
+      if priv.eql?('') || pub.eql?('')
         raise Cryptic::KeyGenerationFailure, "The keypair was never successfully generated"
       end
 
       FileUtils.mkdir_p(File.dirname(path))
 
       File.open("#{File.expand_path(path)}/cryptic_private.pem", 'w') do |file|
-        file.write @private_key
+        file.write priv
       end
 
       File.open("#{File.expand_path(path)}/cryptic_public.pem", 'w') do |file|
-        file.write @public_key
+        file.write pub
       end
 
       path
